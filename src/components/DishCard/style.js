16304412962cfec6068e315390a3379dc4002895
Thiago Koch <tkoch.dev@gmail.com>
2023-12-12 11:27:42 -0300

    feat: start create DishCard

---
+++ src/components/DishCard/style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export const FavoriteOrEditButton = styled.div`
+color: white;
+`
+export const DishImg = styled.img`
+`
+export const DishTitle = styled.button`
+border: none;
+`
+export const DishDrescription = styled.div`
+`
+export const DishPrice = styled.div`
+
+`
+export const DishQuantity = styled.div`
+`
