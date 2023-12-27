b681d2c44f6be39cd42de900827b9646cb8bd54c
Thiago Koch <tkoch.dev@gmail.com>
2023-12-27 17:12:09 -0300

    feat: fetch dishData on DishDetails page to insert in DishContent component

--- src/pages/DishDetails/index.jsx
+++ src/pages/DishDetails/index.jsx
@@ -1,12 +1,27 @@
 import {Container} from './style';
 import { NavBar } from '../../components/NavBar';
 import { Menu } from '../../components/Menu';
-import { useState } from 'react';
 import { Footer } from '../../components/Footer';
+import { DishContent } from '../../components/DishContent';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { api } from '../../services/api';
 
 export function DishDetails() {
 
+  const params = useParams();
+
+  const [dishData, setdishData] = useState();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    async function fetchDish() {
+      const response = await api.get(`/dish/${params.id}`, {withCredentials:true});
+      setdishData(response.data);
+    }
+
+    fetchDish();
+  }, []);
 
   return (
     <Container className='page'>
@@ -16,6 +31,8 @@ export function DishDetails() {
         isMenuOpen = {isMenuOpen}
         closeMenu = {() => setIsMenuOpen(false)}
       />
+
+      {dishData && <DishContent/>}
 
       <div className='footer'>
         <Footer />
